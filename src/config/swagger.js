222e7eeadf372e193b493348aad2b562f948f56f
hager1nady <hajrnady24@gmail.com>
2026-09-05 11:19:14 +0300

    Fix Swagger server URL

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -1,20 +1,21 @@
 const swaggerJsdoc = require("swagger-jsdoc");
 
 const options = {
-    definition: {
-        openapi: "3.0.0",
-        info: {
-            title: "Fixawy API",
-            version: "1.0.0",
-            description: "Fixawy Backend API",
-        },
-        servers: [
-            {
-                url: "http://localhost:5000",
-            },
-        ],
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Fixawy API",
+      version: "1.0.0",
+      description: "Fixawy Backend API",
     },
-    apis: ["./src/modules/**/*.routes.js"],
+    servers: [
+      {
+        url: "https://fixawy-backend.vercel.app",
+      },
+    ],
+  },
+
+  apis: ["./src/modules/**/*.routes.js"],
 };
 
 const swaggerSpec = swaggerJsdoc(options);
