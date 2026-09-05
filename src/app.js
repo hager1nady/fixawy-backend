9c3904d491571ea691bc71b934b38482923ce2ba
hager1nady <hajrnady24@gmail.com>
2026-09-05 11:32:29 +0300

    Fix Swagger UI for Vercel

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 
 const express = require("express");
-const swaggerUi = require("swagger-ui-express");
 
 const clientRoutes = require("./modules/client/client.routes");
 const technicianRoutes = require("./modules/technician/technician.routes");
@@ -19,12 +18,41 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/clients", clientRoutes);
+
 app.use("/api/technicians", technicianRoutes);
 
-app.use(
-    "/api-docs",
-    swaggerUi.serveFiles(swaggerSpec),
-    swaggerUi.setup(swaggerSpec)
-);
+app.get("/api-docs", (req, res) => {
+    res.send(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Fixawy API Docs</title>
+
+            <link
+                rel="stylesheet"
+                href="https://unpkg.com/swagger-ui-dist/swagger-ui.css"
+            />
+        </head>
+
+        <body>
+            <div id="swagger-ui"></div>
+
+            <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
+
+            <script>
+                window.onload = () => {
+                    SwaggerUIBundle({
+                        spec: ${JSON.stringify(swaggerSpec)},
+                        dom_id: "#swagger-ui",
+                        deepLinking: true
+                    });
+                };
+            </script>
+        </body>
+        </html>
+    `);
+});
 
 module.exports = app;
